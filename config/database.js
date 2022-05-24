0b6a26b1083eb8e9afb4c754293f183cd6ec7c5e
Eduardo Cabrera <lalo_cabrera1977@hotmail.com>
2022-05-23 23:32:40 -0500

    Change on database keys

--- config/database.js
+++ config/database.js
@@ -51,11 +51,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: 'ec2-34-201-95-176.compute-1.amazonaws.com',
+      port: '5432',
+      user: 'ovyfpmoupykeyu',
+      password: 'e5f8c4df2d2593668420dcbbe363ef08cd5ee67e038301ca64b135ce31a53b5f',
+      database: 'da63gm11avt5u1'
     },
     debug: Env.get('DB_DEBUG', false)
   },
